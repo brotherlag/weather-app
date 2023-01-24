d6a516115c30229b04fd8bb83a6e1e8cac3cf0b1
brotherlag <117751778+brotherlag@users.noreply.github.com>
2023-01-24 02:50:26 +0200

    stateservice

--- my-app/src/services/stateService.js
+++ my-app/src/services/stateService.js
@@ -10,11 +10,14 @@ const initialState = {
         city: 'Tallinn',
     },
     forecastSelectedData: null,
+    errorMessage: null,
 };
 
 export const setSearchParams = createAction('setSearchParams');
 export const setShowSearchBar = createAction('setShowSearchBar')
 export const setForecastSelectedData = createAction('setForecastSelectedData')
+export const setErrorMessage = createAction('setErrorMessage')
+
 
 const reducer = createReducer(initialState, {
     [setSearchParams]: (iState, action) => {
@@ -25,6 +28,9 @@ const reducer = createReducer(initialState, {
     },
     [setForecastSelectedData]: (iState, action) => {
         iState.forecastSelectedData = action.payload;
+    },
+    [setErrorMessage]: (iState, action) => {
+        iState.errorMessage = action.payload;
     },
 });
 

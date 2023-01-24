d010b7e96e3fb1be196cc207b006d3aba6cdcb86
brotherlag <117751778+brotherlag@users.noreply.github.com>
2023-01-24 02:56:54 +0200

    now

--- my-app/src/Body/Tabs/Now.js
+++ my-app/src/Body/Tabs/Now.js
@@ -1,20 +1,24 @@
 import { useState, useEffect } from "react";
 import Map from "./Map";
 import { getWeather } from "../../services/apiService";
-import ErrorModal from "../../ErrorModal";
 import Data from "./Data";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { setErrorMessage } from "../../services/stateService";
 
 function Now() {
 
-    const [errorMessage, setErrorMessage] = useState(null);
     const [weatherData, setWeatherData] = useState(null);
     
     const searchParams = useSelector((state) => state.searchParams)
+
+    const dispatch = useDispatch();
+
     useEffect(() => {
         (async function () {
             try {
+
                 const response = await getWeather(searchParams);
+                
                 const data = await response.json();
 
                 if (data.cod !== 200) throw Error(data.message);
@@ -22,18 +26,15 @@ function Now() {
                 setWeatherData(data);
             } catch (error) {
                 console.log(error);
-                setErrorMessage(error.message);
+                dispatch(setErrorMessage(error.message));
             }
         })()
-    }, [searchParams]);
+    }, [dispatch, searchParams]);
 
     return (
         <>
             <Data data={weatherData} />
             <Map weatherData={weatherData} />
-            <ErrorModal
-                message={errorMessage}
-                handleClose={() => setErrorMessage(null)} />
         </>
     );
 }

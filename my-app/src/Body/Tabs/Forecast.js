401214deda27f78225678367efd54bdb29a80920
brotherlag <117751778+brotherlag@users.noreply.github.com>
2023-01-24 02:53:48 +0200

    forecast

--- my-app/src/Body/Tabs/Forecast.js
+++ my-app/src/Body/Tabs/Forecast.js
@@ -3,7 +3,8 @@ import TimeSelector from "./TimeSelector";
 import Data from "./Data";
 import Map from "./Map";
 import { getForecast } from "../../services/apiService";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { setErrorMessage } from "../../services/stateService";
 
 function Forecast() {
     const [forecastData, setForecastData] = useState(null);
@@ -11,13 +12,22 @@ function Forecast() {
     const searchParams = useSelector((state) => state.searchParams);
     const forecastSelectedData = useSelector((state) => state.forecastSelectedData);
 
+    const dispatch = useDispatch();
+
     useEffect(() => {
         (async function () {
+            try{
             const response = await getForecast(searchParams);
             const data = await response.json();
+
+            if(+data.cod !== 200) throw Error(data.message);
+
             setForecastData(data);
+            } catch(error) {
+                dispatch(setErrorMessage(error.message));
+            }
         })();
-    }, [searchParams]);
+    }, [dispatch, searchParams]);
 
     const weatherData = forecastData ? {
         ...forecastData?.list[0],
